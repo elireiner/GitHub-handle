95520a229726a7ddb773b7bc67787d40b4ce6dbf
Eli Reiners <goelyukim@gmail.com>
2019-11-19 16:48:53 -0500

    minor update

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 function returnRepos(handle) {
 
     const url = `https://api.github.com/users/${handle}/repos`
+    console.log(url)
     fetch(url)
     .then(response => response.json())
     .then(responseJson => console.log(responseJson))
-    .catch(err => console.log(`error:`, err))
+    .catch(err => alert(`error:`+ err))
 }
 function handleSubmit() {
     $('form').on('click', 'button', function (event) {
