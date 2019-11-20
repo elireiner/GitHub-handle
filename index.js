67dbcb2c9aa02967e8ad87af213453b9d47a4bfc
Eli Reiners <goelyukim@gmail.com>
2019-11-20 00:07:45 -0500

    completed repo listing

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
+'use strict';
 
-function returnRepos(handle) {
+function displayRepos(responseJson, handle) {
+    $('#user-header').empty();
+    $('#results-list').empty();
+    if (responseJson.length > 0) {
+        if (responseJson.length === 1) {
+            $('#user-header').append(`This is ${handle}'s repo`)
+        }
+        else {
+            $('#user-header').append(`These are ${handle}'s repos`)
+        }
+        for (let i = 0; i < responseJson.length; i++) {
+            $('#results-list').append(`<li><h3>${handle} created a repo named: '${responseJson[i].name}
+        '</h3><a href="${responseJson[i].html_url}" 
+        target="_blank">Here is a link to it.</a></li>`)
+        }
+    }
+    else {
+        $('#user-header').append(`${handle} does not have repos yet`)
+    }
+
+
+    $('#results').removeClass('hidden')
+}
+
+function getRepos(handle) {
 
     const url = `https://api.github.com/users/${handle}/repos`
     console.log(url)
     fetch(url)
-    .then(response => response.json())
-    .then(responseJson => console.log(responseJson))
-    .catch(err => alert(`error:`+ err))
+        .then(response => response.json())
+        .then(responseJson => displayRepos(responseJson, handle))
+        .catch(err => alert(`error:` + err))
 }
 function handleSubmit() {
-    $('form').on('click', 'button', function (event) {
+    $('form').submit(function (event) {
         event.preventDefault();
         const handle = $('input').val();
-        returnRepos(handle);
-   })
+        getRepos(handle);
+    })
 }
 
 $(handleSubmit);
